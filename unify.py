73474a27be2cee0356d97034f73282027c567d39
Kevin Nguyen <kevinnguyen@Kevins-MacBook-Pro.local>
2019-02-12 19:59:37 -0800

    Skeleton

---
+++ unify.py
@@ -0,0 +1,14 @@
+def main():
+    isConstant = True
+    isVariable = True
+
+    if E1 == isConstant and E2 == isConstant and E1 == E2:
+        if E1 == E2:
+            return"EMPTY SET"
+    if E1 == isVariable:
+        // if E2 contains E1
+        if E2.find(E1):
+            return "FAIL"
+
+
+main()
